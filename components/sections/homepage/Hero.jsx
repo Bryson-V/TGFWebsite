cf9911cb2ae5edfb3b537d8bbf8061ac0287efd7
Bryson-V <97585647+Bryson-V@users.noreply.github.com>
2026-08-03 09:35:33 +1000

    redirected homepage link

--- components/sections/homepage/Hero.jsx
+++ components/sections/homepage/Hero.jsx
@@ -26,7 +26,7 @@ export default function Hero() {
             We are committed to <strong>getting Guam healthy, one man, one woman, and one child at a time.</strong>
           </h1>
           <p className={styles.subline}>A locally and nationally recognized 501(c)(3) non-profit organization.</p>
-          <Button href="https://toduguam.com/who-we-are/">Learn more about Our Story</Button>
+          <Button href="/ourStory">Learn more about Our Story</Button>
         </div>
 
         <a
