ced6c6e13b94fed059f61f6139a6b787bd3b3a70
Jaden <Leejjaden@gmail.com>
2026-08-03 16:04:06 +1000

    director linked

--- app/boardOfDirectors/page.js
+++ app/boardOfDirectors/page.js
@@ -1,11 +1,5 @@
-// Import other relevant sections for your "Our Story" page here
 
-/**
- * OurStoryPage
- * 
- * The "Our Story" page component featuring the new StoryHero banner
- * and subsequent story-specific sections.
- */
+
 export default function BoardOfDirectorsPage() {
   return (
     <>
