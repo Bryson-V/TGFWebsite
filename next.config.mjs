bd22c776f4f462e5c6bb03152eaf76fb4b0c09f0
Bryson-V <97585647+Bryson-V@users.noreply.github.com>
2026-08-12 16:42:38 +1000

    fixed config.mjs for demo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextJSConfigFile} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+// If repository name is TGFWebsite
+const repoName = 'TGFWebsite';
+
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  // Set the base path to your repository name when deploying on GitHub Actions
+  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true, // Required for static export on GitHub Pages
+  },
 };
 
 export default nextConfig;
