d7c22dadcb0742a6c05a45efd58da3f1b2c648c4
Bryson-V <97585647+Bryson-V@users.noreply.github.com>
2026-08-28 09:32:01 +1000

    fixed asset pathing

--- app/community/commImpact/page.js
+++ app/community/commImpact/page.js
@@ -2,12 +2,13 @@ import CancerCareSupport from "@/components/sections/community/impact/CancerCare
 import ImpactHero from "@/components/sections/community/impact/impactHero";
 import Partner from "@/components/sections/community/impact/partners";
 import PatientDataVisualizer from "@/components/sections/community/impact/PatientDataVisualizer.jsx";
+import { getAssetPath } from "@/lib/utils";
 
 export default function PatientsPage() {
   return (
     <>
-        <ImpactHero />
-      <PatientDataVisualizer filePath="/excel/patient-data.json" />       
+      <ImpactHero />
+      <PatientDataVisualizer filePath={getAssetPath("excel/patient-data.json")} />      
       <Partner />
     </>
   );
